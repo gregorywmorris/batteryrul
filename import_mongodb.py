25972fffc241e631c43f1ead3c83096ce290ac30
gregorywmorris <gwm1980@gmail.com>
2024-08-02 23:27:57 -0400

    data file updated, mongodb import script, notebook updates, pdm updated

---
+++ import_mongodb.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import os
+from dotenv import load_dotenv
+from pymongo import MongoClient
+
+# Load the .env file
+load_dotenv()
+
+# Access the MongoDB password from the environment variable
+mongodb_password = os.getenv('MONGODB_PW')
+
+# Load the CSV file into a DataFrame
+df = pd.read_csv('data/Battery_RUL.csv')
+
+# Convert the DataFrame to a list of dictionaries
+data_dict = df.to_dict(orient='records')
+
+# Create a MongoDB client
+client = MongoClient(f'mongodb+srv://blackitalian:{mongodb_password}@freetier.c43u4hi.mongodb.net/')
+
+# Specify the database and collection
+db = client['battery_experiments']
+collection = db['battery_rul']
+
+# Insert the data into MongoDB
+collection.insert_many(data_dict)
+
+# Confirm insertion
+print("Data inserted successfully")
+
